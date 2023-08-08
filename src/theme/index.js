1db5bac7466a2b3c53fb3459de0b493e33a521ca
fernando.michea@brik.cl <fernando.michea@brik.cl>
2023-08-08 04:30:29 -0400

    cambios

--- src/theme/index.js
+++ src/theme/index.js
@@ -2,36 +2,29 @@
 import React from 'react';
 import CssBaseline from '@mui/material/CssBaseline';
 import { createTheme, ThemeProvider as MuiThemeProvider } from '@mui/material/styles';
-import orange from '@mui/material/colors/orange';
 import { ReactNode } from 'react';
 import {palette} from './palette';
-interface ThemeProviderProps {
-  children: ReactNode;
-}
 
-export const ColorModeContext = React.createContext({ toggleColorMode: () => {} });
+const ColorModeContext = React.createContext({ toggleColorMode: () => {} });
 
-export function ThemeProvider({ children }: ThemeProviderProps) {
-  const [mode, setMode] = React.useState<'light' | 'dark'>('light');
+function ThemeProvider({ children }) {
+  const [mode, setMode] = React.useState('light');
   const colorMode = React.useMemo(
     () => ({
       toggleColorMode: () => {
-        setMode((prevMode) => (prevMode === 'light' ? 'dark' : 'light'));
+        setMode(prevMode => (prevMode === 'light' ? 'dark' : 'light'));
       },
     }),
     []
   );
 
   const theme = React.useMemo(() => {
-  
     return createTheme({
-        palette: {
-          mode: mode,
-          ...(mode === 'light' ? palette("light")  : palette("dark")),
-        },
-      });
-      
-    
+      palette: {
+        mode: mode,
+        ...(mode === 'light' ? palette("light") : palette("dark")),
+      },
+    });
   }, [mode]);
 
   return (
@@ -43,3 +36,8 @@ export function ThemeProvider({ children }: ThemeProviderProps) {
     </ColorModeContext.Provider>
   );
 }
+
+module.exports = {
+  ThemeProvider,
+  ColorModeContext,
+};
