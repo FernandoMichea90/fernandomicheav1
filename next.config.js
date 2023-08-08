b717e97bc45ece7a0b1e6b1abfb96aaf133becee
fernando.michea@brik.cl <fernando.michea@brik.cl>
2023-08-08 13:25:36 -0400

    .env.local

--- next.config.js
+++ next.config.js
@@ -1,7 +1,7 @@
 // /** @type {import('next').NextConfig} */
 const nextConfig = {
     output:"export",
-    basePath: '/fernandomicheav1',
+    basePath: process.env.REACT_APP_BASE_PATH,
 }
 
 module.exports = nextConfig
